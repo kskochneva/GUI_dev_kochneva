22f31a282b3bc55e3368300d83593c772bac64eb
kskochneva <0758299@gmail.com>
2025-10-22 15:27:45 +0300

    final commit

--- max_finder.py
+++ max_finder.py
@@ -1,5 +1,5 @@
-<<<<<<< HEAD
 def max_finder(array):
+    
     if not array:
         return None
     return max(array)
@@ -10,17 +10,4 @@ if __name__ == "__main__":
     for i in range(count):
         k = float(input("Введите число:"))
         array.append(k)
-=======
-def max_finder(array):
-    if not array:
-        return None
-    return max(array)
-    
-if __name__ == "__main__":
-    count = int(input("Введите количество элементов:"))
-    array = []
-    for i in range(count):
-        k = float(input("Введите число:"))
-        array.append(k)
->>>>>>> 285ffe332c47ab37a846c6435f471c97dd9962d0
-    print("Найден максимум:",max_finder(array))
+print("Найден максимум:",max_finder(array))
